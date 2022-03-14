c91af3406902c78a4f80d74199d1b90160b0cde2
Pierre Ablin <pierreablin@gmail.com>
2022-03-14 20:52:57 +0100

    add convergence test

--- tests/test_landing.py
+++ tests/test_landing.py
@@ -5,6 +5,8 @@ import geoopt
 
 from landing import LandingSGD
 
+torch.manual_seed(1)
+
 
 @pytest.mark.parametrize("momentum", [0, 0.5])
 @pytest.mark.parametrize("shape", [(3, 3), (4, 3, 3), (5, 4, 3, 3)])
@@ -16,3 +18,22 @@ def test_forward(shape, momentum, safe_step):
     loss = (param ** 2).sum()
     loss.backward()
     optimizer.step()
+
+
+def test_convergence():
+    p = 3
+    param = geoopt.ManifoldParameter(torch.randn(p, p), manifold=geoopt.Stiefel())
+    with torch.no_grad():
+        param.proj_()
+    optimizer = LandingSGD((param,), lr=.1)
+    n_epochs = 100
+    # Trace maximization: should end up in identity
+    for _ in range(n_epochs):
+        optimizer.zero_grad()
+        loss = -torch.trace(param)
+        loss.backward()
+        optimizer.step()
+        
+    assert loss.item() + p < 1e-5
+    orth_error = torch.norm(param.mm(param.t()) - torch.eye(p))
+    assert orth_error < 1e-5 
